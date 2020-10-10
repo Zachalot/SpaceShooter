65a798b4229625c0efa1a3d1a4cdf141dcb9288d
Zachalot <zdweinberg@gmail.com>
2020-10-10 19:42:23 -0400

    stop spawning enemies on player death

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn_Manager : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
-    private float _spawnFrequency = 5f; // how often we spawn enemies in seconds
+    private float _spawnFrequency = 2f; // how often we spawn enemies in seconds
 
     [SerializeField]
     private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    private bool _spawnEnemies = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +29,10 @@ public class Spawn_Manager : MonoBehaviour
     // Apparently anytime we return an IEnumerator, this is a coroutine ...... so I literally can't user IEnumerators for anything else in unity
     private IEnumerator SpawnEnemies(float spawnFrequency) 
     {
-        while (true)
+        while (_spawnEnemies)
         {
-            Instantiate(_enemyPrefab, new Vector3(RandomX(), 7, 0), Quaternion.identity);
+            GameObject newEnemy = Instantiate(_enemyPrefab, new Vector3(RandomX(), 7, 0), Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(spawnFrequency);
         }
     }
@@ -34,5 +40,10 @@ public class Spawn_Manager : MonoBehaviour
     private float RandomX()
     {
         return Random.Range(-10, 10);
+    }
+
+    public void OnPlayerDeath()
+    {
+        _spawnEnemies = false;
     }
 }
